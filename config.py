67343e2dccf7957437dcc421214b85306e947c47
procrastinatorMuffin <danil.sholudko@icloud.com>
2023-08-08 20:59:09 +0900

    refactor: Move API token to environment variable for security
    - Extracted sensitive API token from config.py and placed it in a .env file.
    - Implemented dotenv to load the API token as an environment variable.
    - Updated .gitignore to ensure .env file is not committed to the repository.

--- config.py
+++ config.py
@@ -1,2 +1,31 @@
-API_TOKEN = 'TG_API_TOKEN'
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Read API_TOKEN from environment variables
+API_TOKEN = os.getenv('API_TOKEN')
 DB_NAME = 'referral_codes.db'
+
+# General Bot Responses
+WELCOME_MSG = "Привет! Отправь мне свой реферальный код командой /add. Используй /povo, чтобы получить случайный " \
+              "реферальный код."
+CODE_ADDED_SUCCESS = "Ваш реферальный код успешно добавлен!"
+CODE_ALREADY_EXISTS = "Такой реферальный код уже существует!"
+NO_CODES_AVAILABLE = "В данный момент реферальные коды отсутствуют."
+RATE_LIMIT_EXCEEDED = "Воспользуйтесь кодом, который вы запросили ранее."
+NOT_AUTHORIZED = "У вас недостаточно прав для использования этой команды."
+CONFIRM_USAGE_PROMPT = "Вы уверены, что хотите пометить этот код как использованный? Сообщение с кодом будет " \
+                       "безвозвратно удалено."
+ACTION_CANCELLED = "Действие отменено."
+REFERRAL_CODE_MSG = "Вот ваш реферальный код: {}"
+CODE_NOT_FOUND = "Реферальный код не найден."
+CODE_DELETED_SUCCESS = "Реферальный код успешно удален!"
+INVALID_OR_DUPLICATE_CODE = "Реферальный код недействителен или уже был добавлен ранее"
+
+# Inline Keyboard Buttons (keeping these the same as they contain universal symbols)
+USED_BUTTON_TEXT = "Я использовал(а) код ✅"
+CONFIRM_BUTTON_TEXT = "Да, я осознаю, что код будет безвозвратно удалён ✅"
+CANCEL_BUTTON_TEXT = "Нет, я случайно ❌"
+
